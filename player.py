ce840e95a9f75bc6bf59135026af5ee46d3bda6c
Kaylamck202 <kaylamck202@gmail.com>
2018-10-11 13:49:48 -0500

    initial commit

---
+++ player.py
@@ -0,0 +1,9 @@
+class Player:
+    def __init__(self,piece):
+        self.name = self.get_name()
+        self.piece = piece
+    def get_name(self):
+        pass
+    
+    def get_choice(self,board):
+        pass
