9516f7826ecaece1f9b3cce93673c0675aad57ce
Kaylamck202 <kaylamck202@gmail.com>
2018-10-11 14:08:11 -0500

    add turn

--- game.py
+++ game.py
@@ -2,4 +2,13 @@ from board import Board
 from player import Player
 
 class Game:
+    def __init__(self):
+        self.turn
+        
+        pass
+    def play_game(self):
+        pass
+
+if __name__=="__main__":
+    #test code
     pass
