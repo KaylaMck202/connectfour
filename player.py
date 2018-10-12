aa27cd6ce6c2fb529cdb508c1896bf5f37043ace
Kaylamck202 <kaylamck202@gmail.com>
2018-10-12 14:05:00 -0500

    add choice

--- player.py
+++ player.py
@@ -1,17 +1,23 @@
+#from board import Board
 class Player:
     def __init__(self,piece):
         self.name = self.get_name()
         self.piece = piece
     def get_name(self):
-        name1 = input("Insert your name: ")
-        name2 = input("Insert your name: ")
+        name1 = input("Insert Player 1 name: ")
+        name2 = input("Insert Player 2 name: ")
+        self.get_choice()
         #pass
     
     def get_choice(self,board):
-        pass
+        choice = int(input("What column do you want to place your chip?:"))
+        col = choice
+            
+        #pass
     
     
     
 if __name__=="__main__":
     #test code
+    #Player()
     pass
