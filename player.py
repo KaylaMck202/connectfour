4e9237b39f397ef9222b46a43d6f7d78997221de
Kaylamck202 <kaylamck202@gmail.com>
2018-10-15 14:10:59 -0500

    wip

--- player.py
+++ player.py
@@ -3,22 +3,17 @@ class Player:
     def __init__(self,piece):
         self.name = self.get_name()
         self.piece = piece
+        #self.choice = self.get_choice()
     def get_name(self):
-        name1 = input("Insert Player 1 name: ")
-        name2 = input("Insert Player 2 name: ")
-        self.get_choice()
-        #pass
+        self.name = input("What is your name?")
     
     def get_choice(self,board):
-        #board= Board()
-        choice = int(input("What column do you want to place your chip?:"))
-        #choice = piece 
-            
-        #pass
+        #choice = int(input("What column do you want to place your chip?:"))
+        pass
     
 def main():   
     pass
 if __name__=="__main__":
     #test code
-    #Player(2)
-    pass
+    Player(2)
+    #pass
