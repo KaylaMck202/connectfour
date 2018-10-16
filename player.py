e71241acb8fa81b0d97094fb806cc1b3b4312b52
Kaylamck202 <kaylamck202@gmail.com>
2018-10-16 14:08:53 -0500

    work on choice

--- player.py
+++ player.py
@@ -1,19 +1,18 @@
-#from board import Board
+from board import Board
 class Player:
     def __init__(self,piece):
-        self.name = self.get_name()
+        self.name = self.get_name
         self.piece = piece
-        #self.choice = self.get_choice()
+        self.choice = self.get_choice
     def get_name(self):
         self.name = input("What is your name?")
-    
+#        p1 = player1
+#        p2
     def get_choice(self,board):
-        #choice = int(input("What column do you want to place your chip?:"))
-        pass
+        choice = int(input(f"{player[turn].name} pick column"))
+        return choice 
+        #pass
     
-def main():   
-    pass
 if __name__=="__main__":
-    #test code
     Player(2)
     #pass
