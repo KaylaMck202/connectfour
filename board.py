cb2962f9caf2caf66473bf0e7fd94c0d82ba221e
VERA.AARON <2200839@fbisd.flourbluffschools.net>
2018-10-16 14:11:42 -0500

    wip

--- board.py
+++ board.py
@@ -13,37 +13,26 @@ class Board:
                 self.board[row][col -1] = piece
                 break
         else:
-            raise ValueError('Column full')
-        
+            raise ValueError('Column Full')
                 
     def empty_board(self):
         self.board = [[' ']*self.width for i in range(self.height)]
         
-    def check_win(self):
-        num_inrow = 0
-        for row in range(self.height):
-            for col in range(self.width -4):
-                if self.board[row][col] == 'x':
-                    num_inrow += 1
-                if not self.board[row][col] == 'x':
-                    num_inrow = 0
-                if num_inrow >= 4:
-                    print('true')  #get rid of in final
-                    return True
-                
-        num_inrow = 0
+    def check_win(self):       
         for row in range(self.height):
             for c in range(self.width -4):
-                if self.board[row][col] == 'o':
-                    num_inrow += 1
-                if not self.board[row][col] == 'o':
-                    num_inrow = 0
-                if c >= 4:
-                    print('true')  #get rid of in final
-                    return True
+                if (self.board[row][c] == self.board[row][c+1] == self.board[row][c+2] == self.board[row][c+3] != ' '):
+                    #print('win')
+                    return True  
+#                else:
+#                    print('not win')
+#                    return False
 
-                
-                     
+        for row in range(self.height):
+            for col in range(self.width):
+                pass
+                            
+                   
     def is_full(self):             
         for row in self.board:
             for element in row:                
@@ -56,15 +45,20 @@ class Board:
             return True
                 
     def disp_board(self):
+#        for row in self.board:
+#            for element in row:
+#                print(element,end=' ')
+#            print()           
+#        for i in range(self.width):
+#            print('_',end=' ')           
+#        print()            
+#        for i in range(self.width):
+#            print(i + 1,end=' ')
+
         for row in self.board:
             for element in row:
                 print(element,end=' ')
             print()
-            
-        for i in range(self.width):
-            print('_',end=' ')
-            
-        print()            
         for i in range(self.width):
             print(i + 1,end=' ')
            
@@ -73,13 +67,14 @@ def main():
     bd.run()
     
 if __name__ == "__main__":
-    bd = Board(6,7)
-    bd.add_piece('x',4)
-    bd.add_piece('x',5)
-    bd.add_piece('x',1)
-    bd.add_piece('x',2)
-    bd.add_piece('x',3)
-    bd.add_piece('i',3)
+    bd = Board(7,6)
+#    bd.add_piece(' ',4)
+#    bd.add_piece(' ',5)
+#    bd.add_piece(' ',1)
+#    bd.add_piece(' ',2)
+#    bd.add_piece(' ',3)
+#    bd.add_piece(' ',3)
+#    bd.add_piece(' ',6)
     bd.disp_board()
-    bd.check_win()
+#    bd.check_win()
 
